23b31fa972ccd2cf5324f016f7d28f9a04222875
pkravi22 <124778460+pkravi22@users.noreply.github.com>
2026-09-08 16:29:33 +0530

    Add new image to gallery in HomeClient.jsx

--- src/app/HomeClient.jsx
+++ src/app/HomeClient.jsx
@@ -21,6 +21,7 @@ const gallery = [
   { src: '/Adobe Scan 08 Sept 2026_page-0001.jpg', title: 'जनसभा' },
   { src: '/Adobe Scan 08 Sept 2026_page-0003.jpg', title: 'महिला कार्यक्रम' },
   { src: '/Adobe Scan 08 Sept 2026_page-0007.jpg', title: 'जनसेवा' },
+    { src: '/Adobe Scan 08 Sept 2026_page-0009.jpg', title: 'जनसेवा' },
   { src: '/Adobe Scan 08 Sept 2026_page-0005.jpg', title: 'BJP कार्यक्रम' },
   { src: '/DSC_1042.JPG.webp', title: 'महिला सशक्तिकरण गोष्ठी' },
   { src: '/DSC_1703.JPG.webp', title: 'विकास कार्य निरीक्षण' },
